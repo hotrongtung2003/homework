0ef5bd788574c73361680d8f793193ee39cadbda
hotrongtung2003 <hotrongtung2003@gmail.com>
2023-07-22 00:00:36 +0700

    phanso

--- phanso.cpp
+++ phanso.cpp
@@ -6,77 +6,32 @@ class phanso
 		int tuso;
 		int mauso;
 	public:
-		phanso(int tuso=0, int mauso=0)
-			{
-				this->tuso=tuso;
-				this->mauso=mauso;
-			}
-		~phanso()
-			{
-				
-			}
-		void nhap()
-			{
-				cout<<"\tNhap tu so : ";
-				cin>>tuso;
-				cout<<"\tNhap mau so : ";
-				cin>>mauso;
-			}
-		int UCLN(int a, int b)
-			{
-				//truong hop a < b !!!!
-				if(a + b == a or a + b == b)
-					{
-						return a+b;
-					}
-				else if(a < b)
-					{
-						for(int i=a; i > 1 ; i--)
-							{
-								if(b%i == 0 and a%i == 0)
-									{
-										return i;
-									}
-							}
-					}
-				else
-					{
-						for(int i=b; i > 1 ; i--)
-							{
-								if(b%i == 0 and a%i == 0)
-									{
-										return i;
-									}
-							}
-					}
-			}
-		void tong(phanso ps)
-			{
-				phanso a;
-				a.tuso = this->tuso * ps.mauso + this->mauso * ps.tuso;
-				a.mauso = this->mauso * ps.mauso;
-				int ucln = UCLN(a.tuso, a.mauso);
-				if(a.tuso % a.mauso == 0)
-					{
-						cout<<"Tong hai phan so la : "<<(a.tuso/ucln)/(a.mauso/ucln);
-					}
-				else
-					{
-						cout<<"Tong hai phan so la : "<<a.tuso/ucln<<"/"<<a.mauso/ucln;
-					}
-			}
-		void xuat()
-			{
-				cout<<tuso<<"/"<<mauso;
-			}
+		void nhap();
+		void xuat();;	
 };
+void phanso::nhap()
+{
+	cout<<"Nhap tu so : ";
+	cin>>tuso;
+	do
+		{
+			cout<<"Nhap mau so : ";
+			cin>>mauso;
+			if(mauso==0)
+				{
+					cout<<"Xin vui long nhap mau so khac 0 !!!"<<endl;
+				}
+		}
+	while(mauso==0);
+}
+void phanso::xuat()
+{
+	cout<<"Phan so : "<<tuso<<"/"<<mauso;
+}
 int main()
 {
-	phanso ps1, ps2;
-	cout<<"Nhap phan so thu nhat : "<<endl;
-	ps1.nhap();
-	cout<<"Nhap phan so thu hai : "<<endl;
-	ps2.nhap();
-	ps1.tong(ps2);
+	phanso ps;
+	ps.nhap();
+	ps.xuat();
 	return 0;
 }
